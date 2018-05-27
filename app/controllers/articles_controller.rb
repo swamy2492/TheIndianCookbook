05206eba63048e775888fc1cfe6204b53695ac41
Narayanswamy <swamy2492@gmail.com>
2018-05-28 01:15:48 +0530

    Adding new files for edit and show, also list updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,13 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def index
+    @articles = Article.all
+  end
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -13,6 +19,19 @@ class ArticlesController < ApplicationController
     else
 
       render 'new'
+    end
+    #@article = Article.new(article_params)
+    #@article.save
+  #  redirect_to article_path(@article)
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+
+      render 'edit'
     end
     #@article = Article.new(article_params)
     #@article.save
